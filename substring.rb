02d56e5260f100b84d9de835625d69380c3a25b9
Erika Keith <erikakeith13@gmail.com>
2018-09-29 15:20:14 +0000

    added substrings

---
+++ substring.rb
@@ -0,0 +1,11 @@
+def main
+    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    puts "Give me a word"
+    word = gets.chomp
+    substring(word, dictionary)
+end
+
+def substring(word, dictionary)
+    puts "hello mom"
+end
+main if __FILE__ == $PROGRAM_NAME
